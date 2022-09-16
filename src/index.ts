c97f16bc9a69901648285f09dead97b6f3a876a3
Michael Simkin <michael.simkin22@gmail.com>
2022-09-16 11:53:58 +0300

    feat: Add dat.gui

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { GUI } from "dat.gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module.js";
@@ -35,6 +36,16 @@ const material = new THREE.MeshBasicMaterial({
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube");
+cubeFolder.add(cube.scale, "x", 0, 5, 0.1);
+cubeFolder.add(cube.scale, "y", 0, 5, 0.1);
+cubeFolder.add(cube.scale, "z", 0, 5, 0.1);
+cubeFolder.open();
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 10);
+cameraFolder.open();
 
 const animate: FrameRequestCallback = (time) => {
     const seconds = time / 1000;
